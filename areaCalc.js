03226f48f9fa2d1608d4d0955bb1a3fa7ffc3b9c
YuliiaMatich <yuliia.matich@gmail.com>
2022-06-08 18:55:08 -0400

    completed additional exercises

---
+++ areaCalc.js
@@ -0,0 +1,34 @@
+function calculateRectangleArea(length, width){
+  let Rectangle = "";
+  if (length >=0 && width >= 0){
+  Rectangle = length * width;
+  return Rectangle;
+  }else {undefined}
+}
+
+function calculateTriangleArea(base, height){
+  let triangle = "";
+  if (base >=0 && height>=0) {
+    triangle = base * height / 2;
+    return triangle;
+  } else {undefined}
+}
+
+function calculateCircleArea(radius){
+  if (radius >=0){
+  let circle = Math.Pi * radius * radius;
+  return circle; 
+  }else {undefined}
+}
+
+console.log(calculateRectangleArea(10, 5));     // should print 50
+console.log(calculateRectangleArea(1.5, 2.5));  // should print 3.75
+console.log(calculateRectangleArea(10, -5));    // should print undefined
+
+console.log(calculateTriangleArea(10, 5)); // should print 25
+console.log(calculateTriangleArea(3, 2.5)); // should print 3.75
+console.log(calculateTriangleArea(10, -5)); // should print undefined
+
+console.log(calculateCircleArea(10)); // should print 314.159...
+console.log(calculateCircleArea(3.5)); // should print 38.484...
+console.log(calculateCircleArea(-1)); // should print undefined
---
+++ howManyHundreds.js
@@ -0,0 +1,5 @@
+function howManyHundreds (num){
+num = (num / 100)
+return num = Math.trunc(num);
+}
+console.log(howManyHundreds(1234));
